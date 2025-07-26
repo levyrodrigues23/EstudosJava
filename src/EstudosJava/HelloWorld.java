70e5afbe87ea9b6920e04b0ea7a7c4097082df34
José Levy Rodrigues da Silva <joselevy@alu.ufc.br>
2025-07-26 16:50:00 -0300

    feat: this looks java :)

---
+++ src/EstudosJava/HelloWorld.java
@@ -0,0 +1,17 @@
+
+//aqui é a pasta do projeto
+package EstudosJava;
+
+//todo programa de java é uma classe
+//o nome da classe é o mesmo nome do arquivo
+public class HelloWorld {
+// precisamos criar um método
+    //static vem de estatico, void é um retorno
+    // main é o nome do metodo principal
+    // args é como se fosse o nome do array
+public static void main(String[] args) {
+
+    // system out é o objeto de saida padrão
+    System.out.print("olá mundo ç");
+}
+}
